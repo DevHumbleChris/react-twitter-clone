bf199ae4f491829433087a9e8ddea0c227e47e8c
DevHumbleChris <dev.humblechris254@gmail.com>
2022-12-30 10:11:20 +0300

    Added Sidebar component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <main class="grid grid-cols-6 sm:grid-cols-8 gap-2 space-x-2">
+        <Sidebar />
+    </main>
   )
 }
