118b6c07b56e91d919ed58f63a90aaf3fd30840a
DevHumbleChris <dev.humblechris254@gmail.com>
2022-12-30 12:07:56 +0300

    added tweets loading

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,31 @@
+import {
+  ChatBubbleOvalLeftIcon,
+  HeartIcon,
+  ArrowUpTrayIcon,
+  ArrowsUpDownIcon,
+} from "@heroicons/react/20/solid";
+import React from "react";
+
+export default function Tweet({ tweet }) {
+  return (
+    <div className="border border-gray-300 p-2">
+      <div className="flex space-x-2">
+        <img
+          src={tweet.user.photoURL}
+          alt="user-logo"
+          className="w-16 h-16 border border-gray-200 rounded-full"
+        />
+        <div>
+          <h1>{tweet.user.name}</h1>
+          <p className="text-gray-600 my-2">{tweet.tweet}</p>
+        </div>
+      </div>
+      <div className="flex justify-evenly m-2">
+        <ChatBubbleOvalLeftIcon className="w-6 h-6 text-[#1ca0f2]" />
+        <HeartIcon className="w-6 h-6 text-[#1ca0f2]" />
+        <ArrowsUpDownIcon className="w-6 h-6 text-[#1ca0f2]" />
+        <ArrowUpTrayIcon className="w-6 h-6 text-[#1ca0f2]" />
+      </div>
+    </div>
+  );
+}
