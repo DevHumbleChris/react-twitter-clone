9ffa442efb0c2ded3aeed51836c4868a3e144be2
DevHumbleChris <dev.humblechris254@gmail.com>
2023-01-12 19:06:43 +0300

    fixed tweet post errors

--- src/views/TweetPost.jsx
+++ src/views/TweetPost.jsx
@@ -27,7 +27,10 @@ export default function TweetPost() {
   const [tweet, setTweet] = useState(null);
   const [comments, setComments] = useState([]);
   const [likes, setLikes] = useState([]);
+  const [liked, setLiked] = useState(false);
   const [retweets, setRetweets] = useState([]);
+  const [isRetweeted, setIsReTweeted] = useState(false);
+  const [tweetLoading, setTweetLoading] = useState(true);
   const [user] = useAuthState(auth);
 
   useEffect(() => {
@@ -71,6 +74,7 @@ export default function TweetPost() {
     if (docSnap.exists()) {
       setTweet({ ...docSnap.data(), id: docSnap.id });
     }
+    console.log(tweet);
   };
 
   useEffect(() => {
@@ -87,79 +91,97 @@ export default function TweetPost() {
         </Link>
         <h1>Tweet</h1>
       </div>
-      <div className="p-3">
-        <div className="flex gap-x-3 relative my-2">
-          <span className="w-0.5 h-full z-[-1] absolute left-5 top-11 bg-gray-600"></span>
-          <img
-            src={tweet?.user.photoURL}
-            alt=""
-            className="h-11 w-11 rounded-full"
-          />
-          <div>
-            <div className="inline-block group">
-              <div className="flex items-center space-x-2 justify-between">
-                <h4 className="font-bold text-[15px] sm:text-base">
-                  {tweet?.user.name}
-                </h4>
-                <div className="w-2 h-2 bg-gray-600 rounded-full"></div>
-                <span className="text-[10px] sm:text-base">heelo</span>
-              </div>
-              <div>
-                <p className="text-gray-600 my-2">{tweet?.tweet}</p>
-                {tweet?.image && (
-                  <div className="my-2">
-                    <img
-                      src={tweet?.image}
-                      alt={tweet?.id}
-                      className="object-contain rounded-xl"
-                    />
+      {tweet ? (
+        <div className="p-3">
+          <div className="flex gap-x-3 relative my-2">
+            <span className="w-0.5 h-full z-[-1] absolute left-5 top-11 bg-gray-600"></span>
+            <img
+              src={tweet?.user.photoURL}
+              alt=""
+              className="h-11 w-11 rounded-full"
+            />
+            <div>
+              <div className="inline-block group">
+                <div className="flex items-center space-x-2 justify-between">
+                  <h4 className="font-bold text-[15px] sm:text-base">
+                    {tweet?.user.name}
+                  </h4>
+                  <div className="w-2 h-2 bg-gray-600 rounded-full"></div>
+                  <span className="text-[10px] sm:text-base">heelo</span>
+                </div>
+                <div>
+                  <p className="text-gray-600 my-2">{tweet?.tweet}</p>
+                  {tweet?.image && (
+                    <div className="my-2">
+                      <img
+                        src={tweet?.image}
+                        alt={tweet?.id}
+                        className="object-contain rounded-xl"
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="flex justify-evenly m-2">
+                  <div
+                    className="flex items-center space-x-1 cursor-pointer"
+                    onClick={(e) => commentPost(e)}
+                  >
+                    <ChatBubbleOvalLeftIcon className="w-6 h-6 text-[#1ca0f2]" />
+                    {comments.length > 0 && <p>{comments.length}</p>}
                   </div>
-                )}
-              </div>
-              <div className="flex justify-evenly m-2">
-                <div
-                  className="flex items-center space-x-1 cursor-pointer"
-                  onClick={(e) => commentPost(e)}
-                >
-                  <ChatBubbleOvalLeftIcon className="w-6 h-6 text-[#1ca0f2]" />
-                  {comments.length > 0 && <p>{comments.length}</p>}
-                </div>
-                <div
-                  className="flex items-center space-x-1 cursor-pointer"
-                  onClick={(e) => likePost(e)}
-                >
-                  {liked ? (
-                    <HeartIconFilled className="text-[#f60100] w-6" />
-                  ) : (
-                    <HeartIcon className="w-6 h-6 text-[#1ca0f2]" />
+                  <div
+                    className="flex items-center space-x-1 cursor-pointer"
+                    onClick={(e) => likePost(e)}
+                  >
+                    {liked ? (
+                      <HeartIconFilled className="text-[#f60100] w-6" />
+                    ) : (
+                      <HeartIcon className="w-6 h-6 text-[#1ca0f2]" />
+                    )}
+                    {likes.length > 0 && <p>{likes.length}</p>}
+                  </div>
+                  {tweet?.user.uid === user.uid && (
+                    <TrashIcon
+                      className="text-[#f60100] w-6 cursor-pointer"
+                      onClick={() => deleteTweet(tweet)}
+                    />
                   )}
-                  {likes.length > 0 && <p>{likes.length}</p>}
-                </div>
-                {tweet.user.uid === user.uid && (
-                  <TrashIcon
-                    className="text-[#f60100] w-6 cursor-pointer"
-                    onClick={() => deleteTweet(tweet)}
-                  />
-                )}
-                <div
-                  className="flex items-center space-x-1 cursor-pointer"
-                  onClick={(e) => retweetPost(e)}
-                >
-                  {isRetweeted ? (
-                    <ArrowsUpDownIconFilled className="w-6 h-6 text-green-700" />
-                  ) : (
-                    <ArrowsUpDownIcon className="w-6 h-6 text-[#1ca0f2]" />
-                  )}
-                  {retweets.length > 0 && <p>{retweets.length}</p>}
+                  <div
+                    className="flex items-center space-x-1 cursor-pointer"
+                    onClick={(e) => retweetPost(e)}
+                  >
+                    {isRetweeted ? (
+                      <ArrowsUpDownIconFilled className="w-6 h-6 text-green-700" />
+                    ) : (
+                      <ArrowsUpDownIcon className="w-6 h-6 text-[#1ca0f2]" />
+                    )}
+                    {retweets.length > 0 && <p>{retweets.length}</p>}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="flex mt-4 space-x-3 w-full">
+            <img
+              src={user?.photoURL}
+              alt=""
+              className="h-11 w-11 rounded-full"
+            />
+          </div>
         </div>
-        {/* <div className="flex mt-4 space-x-3 w-full">
-          <img src={user.photoURL} alt="" className="h-11 w-11 rounded-full" />
-        </div> */}
-      </div>
+      ) : (
+        <div className="mx-auto my-12 animate-bounce">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            fill="#1ca0f2"
+            className="w-12 h-12 mx-auto"
+          >
+            <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
+          </svg>
+          <p className="text-center my-2">Loading Tweet...</p>
+        </div>
+      )}
     </section>
   );
 }
